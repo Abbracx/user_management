a7a6d98f702053ef48efa5d28fee2ea87f6768d3
abbracx <tankoraphael@gmail.com>
2025-03-30 15:57:54 +0100

    Applied isort

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
 from djoser.views import UserViewSet
+from rest_framework.filters import OrderingFilter, SearchFilter
+
 from apps.users.pagination import UserPagination
 
 
--- user_management/urls.py
+++ user_management/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from django.urls import include, path
 from rest_framework import permissions
-from django.views.generic import RedirectView
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,7 +25,6 @@ urlpatterns = [
     path("superadmin/", admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
-    # path("api/v1/profile/", include("apps.profiles.urls")),
     path(
         "api/v1/auth/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
